17e6894699c5d51d6fdcc45a45c2caa5df02db86
Daniel Ohlsson <dohlsson89@gmail.com>
2020-07-10 10:34:58 +0200

    Basic recipe evaluation

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+mod model;
+
+use model::*;
+use ron::de::from_reader;
+use std::fs::File;
+use std::io::BufReader;
+
+fn main() {
+    let resources = load_resources("data/resources.ron");
+    let recipes = load_recipes("data/recipes.ron");
+
+    println!("Imported resources:");
+    for resource in &resources {
+        println!("{:?}", resource);
+    }
+    println!();
+
+    let data = Data::new(resources, recipes);
+    data.evaluate_recipes();
+}
+
+fn load_resources(path: &str) -> Vec<Resource> {
+    let file = File::open(path).unwrap();
+    let reader = BufReader::new(file);
+    return from_reader(reader).unwrap();
+}
+
+fn load_recipes(path: &str) -> Vec<Recipe> {
+    let file = File::open(path).unwrap();
+    let reader = BufReader::new(file);
+    return from_reader(reader).unwrap();
+}
---
+++ src/model.rs
@@ -0,0 +1,75 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Copy, Clone, Serialize, Deserialize, Debug, PartialEq)]
+pub enum ResourceType {
+    Coal,
+    Graphite,
+    Last, // Always last, used to calculate the length of this enum
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum Source {
+    Craft,
+    Mine,
+}
+
+pub struct Data {
+    resources: Vec<Resource>,
+    recipes: Vec<Recipe>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Resource {
+    resource_type: ResourceType,
+    source: Source,
+    price: i32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Recipe {
+    creates: Vec<(i32, ResourceType)>,
+    consumes: Vec<(i32, ResourceType)>,
+    time: i32,
+}
+
+impl Recipe {
+    pub fn evaluate(&self) {
+        //println!("asdf {:?}", self.creates.1 as i32);
+    }
+}
+
+impl Data {
+    pub fn new(resources: Vec<Resource>, recipes: Vec<Recipe>) -> Self {
+        Data { resources, recipes }
+    }
+
+    pub fn evaluate_recipes(&self) {
+        for recipe in &self.recipes {
+            self.evaluate_recipe(recipe);
+        }
+    }
+
+    fn evaluate_recipe(&self, recipe: &Recipe) {
+        let mut created_value = 0;
+
+        for r in &recipe.creates {
+            let res = self.get_resource(&r.1);
+            created_value += res.price * r.0;
+        }
+
+        for r in &recipe.consumes {
+            let res = self.get_resource(&r.1);
+            created_value -= res.price * r.0;
+        }
+
+        println!("{:?}", recipe);
+        println!("  created_value: {}", created_value);
+    }
+
+    fn get_resource(&self, rtype: &ResourceType) -> &Resource {
+        self.resources
+            .iter()
+            .find(|r| *rtype == r.resource_type)
+            .unwrap()
+    }
+}
