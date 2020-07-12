ddb69636965eee0ef94a3504f72d757204df0841
Daniel Ohlsson <dohlsson89@gmail.com>
2020-07-12 11:15:03 +0200

    Refactored data loading into one function

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,8 @@ use std::fs::File;
 use std::io::BufReader;
 
 fn main() {
-    let resources = load_resources("data/resources.ron");
-    let recipes = load_recipes("data/recipes.ron");
+    let resources = load_data::<Resource>("data/resources.ron");
+    let recipes = load_data::<Recipe>("data/recipes.ron");
     let data = Data::new(resources, recipes);
 
     println!("\nImported resources:");
@@ -22,19 +22,7 @@ fn main() {
     data.evaluate_recipes();
 }
 
-fn load_resources(path: &str) -> Vec<Resource> {
-    let file = File::open(path).unwrap();
-    let reader = BufReader::new(file);
-    return match from_reader(reader) {
-        Ok(x) => x,
-        Err(e) => {
-            eprintln!("Error deserializing: {}", e);
-            std::process::exit(1);
-        }
-    };
-}
-
-fn load_recipes(path: &str) -> Vec<Recipe> {
+fn load_data<T: serde::de::DeserializeOwned>(path: &str) -> Vec<T> {
     let file = File::open(path).unwrap();
     let reader = BufReader::new(file);
     return match from_reader(reader) {
